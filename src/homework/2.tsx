d8ab55f31edfdf4b93b7a616b9c83ff2345c171d
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2023-12-24 10:47:18 +0100

    task 1,2

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,23 @@
 import React, {useReducer} from "react";
+// Ваше завдання – додати типи для наступних елементів коду:
+
+// RequestStep: Це рядковий літерал.
+
+// State: Цей тип являє собою об'єкт з двома властивостями isRequestInProgress і RequestStep
+
+// Action: Це тип, що представляє можливі дії, які можуть бути відправлені до редюсера.
+
+// Дивіться код і опишіть для нього правильні типи.
+type RequestStep = 'start'| 'pending'| 'finished'| 'idle';
+type State = {
+  isRequestInProgress:boolean;
+  requestStep:RequestStep
+}
+type Action= 
+  | {type:'START_REQUEST'}
+  | {type:'PENDING_REQUEST'}
+  | {type:'FINISH_REQUEST'}
+  | {type: 'RESET_REQUEST'};
 
 const initialState: State = {
   isRequestInProgress: false,
